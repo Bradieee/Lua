8045bbd3bdc9ec0349217a29a1974fca4a08e6d9
Bradieee <85720084+Bradieee@users.noreply.github.com>
2021-07-22 00:04:55 +0800

    Update discord_webhook_1.lua

--- discord_webhook_1.lua
+++ discord_webhook_1.lua
@@ -4,27 +4,31 @@
 local Discord = require('gamesense/discord_webhooks')
 
 -- Variables
-local Webhook = Discord.new('https://ptb.discord.com/api/webhooks/867034223578841128/mR0qSTZhPOf3sZ-UeB1DtST7ArEEjqORUVcQugRJsqlUPuR2kmEEtW6w3NmNUVKOarJ4')
+local Webhook = Discord.new('')
 local RichEmbed = Discord.newEmbed()
 
--- Get Time (Added by me)
+-- Get Time (added by me)
 local client_system_time
 local sys_time = { client.system_time() }
 local actual_time = string.format('%02d:%02d:%02d', sys_time[1], sys_time[2], sys_time[3])
 
---Get Steam username (Added by me)
+--Get Steam username (added by me)
 local name = entity.get_player_name(1)
 
+--Get steamID3 (added by me)
+local steam = entity.get_steam64(1)
+
 -- Properties
-Webhook:setUsername('LUA NAME')
+Webhook:setUsername('')
 Webhook:setAvatarURL('')
 
-RichEmbed:setTitle('Script Name')
-RichEmbed:setDescription('User has loaded your script')
+RichEmbed:setTitle('Title')
+RichEmbed:setDescription('Description')
 RichEmbed:setThumbnail('')
 RichEmbed:setColor(7447295)
 RichEmbed:addField('Time', actual_time, true)
 RichEmbed:addField('User', name, true)
+RichEmbed:addField('SteamID3', steam, true)
 
 -- Send it!
 Webhook:send(RichEmbed)
